58f3ecbd6549eccb81f5640ee9516706576d15ce
Deepak Kumar <deepakkumarnd@gmail.com>
2014-04-01 13:41:58 +0530

    use compatible capistrano puma gem, puma config

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ end
 
 group :development do
   gem 'capistrano', '~> 3.1.0'
-  gem 'capistrano3-puma', require: false
+  gem 'capistrano-puma', require: false
   gem 'capistrano-rails',   '~> 1.1', require: false
   gem 'capistrano-bundler', '~> 1.1', require: false
   gem 'capistrano-rvm',   '~> 0.1', require: false
--- config/puma/staging.rb
+++ config/puma/staging.rb
@@ -4,8 +4,8 @@ workers 1
 daemonize
 quiet
 
-pidfile '/home/deploy/apps/deepak_blog/shared/pids/deepak_blog.deepak_blog'
-state_path '/home/deploy/apps/deepak_blog/shared/sockets/deepak_blog.state'
-bind 'unix:///home/deploy/apps/deepak_blog/shared/sockets/deepak_blog.sock'
+pidfile '/home/deploy/apps/deepak_blog/shared/tmp/pids/deepak_blog.deepak_blog'
+state_path '/home/deploy/apps/deepak_blog/shared/tmp/sockets/deepak_blog.state'
+bind 'unix:///home/deploy/apps/deepak_blog/shared/tmp/sockets/deepak_blog.sock'
 
 preload_app!
