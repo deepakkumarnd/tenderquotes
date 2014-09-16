dd27edb6bd3a7f23b2f2c063b09f6a55a442f29a
Deepak Kumar <deepakkumarnd@gmail.com>
2014-09-16 16:44:07 +0530

    access latest quote first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.page(params[:page]).per(20)
+    @posts = Post.order('created_at DESC').page(params[:page]).per(20)
   end
 
   def show
