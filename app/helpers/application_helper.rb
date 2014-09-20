c30e3082941ec963dcd0c9c5102b794bb26c28c2
Deepak Kumar <deepakkumarnd@gmail.com>
2014-09-20 18:38:29 +0530

    remove unwanted code, and libraries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def markdown(text)
-    MarkdownRenderer.new(text).render
-  end
 end
--- app/renderers/post_renderer.rb
+++
@@ -1,9 +0,0 @@
-class PostRenderer < Redcarpet::Render::HTML
-  def initialize(options)
-    super(options)
-  end
-
-  def block_code(code, language)
-    Pygments.highlight(code, lexer: language, options: { linespans: 'line' })
-  end
-end
